866657399afc9611cdaa191be19a66eb3f7723c9
ibkrm <bkram.stha@gmail.com>
2025-11-17 22:44:11 -0500

    Revert "Fix league creation error - Add season format validation"

--- app/leagues/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LeagueForm } from "@/components/league-form";
-
-export default function NewLeaguePage() {
-  return (
-    <div className="container mx-auto py-10 max-w-2xl">
-      <LeagueForm />
-    </div>
-  );
-}
